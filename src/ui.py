3f664156754773f29063652fea1b1b4b818cbf6e
Basel Ayman <baselaymansalah@gmail.com>
2024-03-04 00:51:01 +0200

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,1 +1,15 @@
 # User interface setup using Gradio
+
+
+import gradio as gr
+from document_processor import upload_document, process_document
+from question_answering import answer_question
+
+
+interface = gr.Interface(
+    fn=answer_question,
+    inputs=[gr.File(label="Upload Document"), "text"],
+    outputs="text",
+    title="Question Answering System",
+)
+interface.launch()
