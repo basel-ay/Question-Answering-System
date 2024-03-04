7c0e627d026da25d075b19470b9ecacc132e2633
Basel Ayman <baselaymansalah@gmail.com>
2024-03-04 22:40:22 +0200

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,14 +1,21 @@
 # User interface setup using Gradio
 
+# Import necessary libraries and functions
 import gradio as gr
-from document_processor import upload_document, process_document
-from question_answering import answer_question
+from question_answering import answer_question  # Import function for answering questions
 
 
+# Define the Gradio interface
 interface = gr.Interface(
-    fn=answer_question,
-    inputs=[gr.File(label="Upload Document"), "text"],
-    outputs="text",
-    title="Question Answering System",
+    fn=answer_question,  # Set the function to be executed
+    inputs=[
+        gr.File(label="Upload Document"),
+        "text",
+    ],  # Define the input components (upload document and text question)
+    outputs="text",  # Define the output component (text answer)
+    title="Question Answering System",  # Set the title of the interface
 )
+
+
+# Launch the Gradio interface
 interface.launch()
