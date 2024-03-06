041a3ace3348a7a6ec58bfcabc527f7361cfba00
Basel Ayman <baselaymansalah@gmail.com>
2024-03-06 19:04:06 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# Main function to run the system with interface setup using Gradio
+
+# Import necessary libraries and functions
+import gradio as gr  
+from src.question_answering import answer_question # Import function for answering questions
+
+# Define the Gradio interface
+interface = gr.Interface(
+    fn=answer_question,  # Set the function to be executed
+    inputs=[
+        gr.File(label="Upload Document"),
+        "text",
+    ],  # Define the input components (upload document and text question)
+    outputs="text",  # Define the output component (text answer)
+    title="Question Answering System",  # Set the title of the interface
+)
+
+
+# Launch the Gradio interface
+interface.launch()
