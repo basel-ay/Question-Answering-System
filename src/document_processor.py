445e780c14c9fd3ae1028f16aa5d11f0a6aff9fd
Basel Ayman <baselaymansalah@gmail.com>
2024-03-08 20:48:30 +0200

    Update document_processor.py

--- src/document_processor.py
+++ src/document_processor.py
@@ -13,20 +13,21 @@ def preprocess_document(doc_bytes):
     """
     try:
         # Open the document file and read its content
-        with open(doc_bytes, "r") as my_file:
+        with open(doc_bytes, "r", encoding="utf-8") as my_file:
+            # Read the content of the file
             text = my_file.read()
-            # Additional cleaning steps can be added here (optional)
 
-            return text
+            # Additional cleaning steps can be added here
+
+            # Replace non-printable characters with space
+            raw_text = ''.join(char if char.isprintable() else ' ' for char in text)
+
+            return raw_text
 
     except FileNotFoundError:
         # Handle file not found error
-        # print("Error: File not found.")
-
         return "Error: File not found."
 
     except Exception as e:
         # Handle any other errors gracefully and return an error message
-        # print(f"Error: {str(e)}")
-
         return f"Error: {str(e)}"
