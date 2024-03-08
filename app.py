05c68fcb08768b1ceacd4a80c4eae0e7c2653546
Basel Ayman <baselaymansalah@gmail.com>
2024-03-08 18:31:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,15 @@
 import gradio as gr  
 from src.question_answering import answer_question # Import function for answering questions
 
+
+# Define the rules
+rules_text = """
+## Follow the below rules:
+1. Upload a non-empty text document format (.txt).
+2. Enter a valid question format with two or more words in the text box provided.
+3. Click the 'Submit' button to get the answer.
+"""
+
 # Define the Gradio interface
 interface = gr.Interface(
     fn=answer_question,  # Set the function to be executed
@@ -13,6 +22,7 @@ interface = gr.Interface(
     ],  # Define the input components (upload document and text question)
     outputs="text",  # Define the output component (text answer)
     title="Question Answering System",  # Set the title of the interface
+    description=rules_text,  # Set the description of the interface
 )
 
 
